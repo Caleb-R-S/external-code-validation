a55d365f778ecf8364415962220ca3fde14dc36e
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 11:48:46 -0600

    Work on API gateway task

--- tasks/api_gateway_task/index.py
+++ tasks/api_gateway_task/index.py
@@ -10,7 +10,7 @@ class MissingLambdaARNException(Exception): pass
 
 class ApiGatewayTask(ValidationTask):
     def api_filter(self, terraform_module):
-        print(terraform_module)
+        print(terraform_module['source'])
         return terraform_module['source'] == 'app.terraform.io/ICS/apigateway-simple-rest-lambda/aws'
 
     def print_start_message(self):
