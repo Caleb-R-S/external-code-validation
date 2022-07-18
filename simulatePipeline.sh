30fedfeef295e3680d113eaf6f123a1301af982e
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 09:12:01 -0600

    copy of pipeline

---
+++ simulatePipeline.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Caleb-R-S/external-code-validation pipelines/external-code-validation
+python pipelines/external-code-validation/index.py
+rm -rf pipelines/external-code-validation
