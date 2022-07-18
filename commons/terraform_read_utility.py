563a2957c7cbcfbba513b314c564fc142b744a7d
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 11:21:23 -0600

    Work on API gateway task

--- commons/terraform_read_utility.py
+++ commons/terraform_read_utility.py
@@ -3,6 +3,7 @@ from commons.validation_tools import get_tf_module
 def yieldNextModule(terraform_dictionaries, filter):
     for terraform_file, terraform_dict in terraform_dictionaries.items():
         terraform_module = get_tf_module(terraform_dict)
+        print(terraform_module)
         if filter(terraform_module):
             yield terraform_module
 
