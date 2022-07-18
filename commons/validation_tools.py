de384aa9d9e27f10818e37384c7ff18c29f0b4e8
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 16:32:39 -0600

    Work on generalization

--- commons/validation_tools.py
+++ commons/validation_tools.py
@@ -40,8 +40,8 @@ def get_list_of_lambda_paths():
     lambda_paths = []
     for (dirpath, dirnames, filenames) in os.walk(generate_location(3) + get_main_yaml_vars()['path_to_lambdas']):
         print(dirpath)
-        if os.path.exists(dirpath +"/index.py"):
-            lambda_paths.append(dirpath.split("lambdas" + os.sep)[1])
+        # if os.path.exists(dirpath +"/index.py"):
+            # lambda_paths.append(dirpath.split("lambdas" + os.sep)[1])
     return lambda_paths
 
 def get_dict_of_terraform_dicts():
