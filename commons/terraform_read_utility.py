cf909a8b928eaea583326ebb9a995db0520060fe
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 10:53:17 -0600

    Determine terraform obj shape

--- commons/terraform_read_utility.py
+++ commons/terraform_read_utility.py
@@ -1,8 +1,8 @@
-from validation_tools import get_tf_module 
+import validation_tools as vt
 
 def yieldNextModule(terraform_dictionaries, filter):
     for terraform_file, terraform_dict in terraform_dictionaries.items():
-        terraform_module = get_tf_module(terraform_dict)
+        terraform_module = vt.get_tf_module(terraform_dict)
         if filter(terraform_module):
             yield terraform_module
 
