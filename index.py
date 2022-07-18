7b9c2db237d725be34e3e22e2a958b47b103f422
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 13:39:41 -0600

    Work on Duplicate Names

--- index.py
+++ index.py
@@ -1,15 +1,15 @@
 from asyncio import tasks
-# from tasks.glp_task.index import GLPTask
+from tasks.api_gateway_task.index import ApiGatewayTask
 from tasks.check_json_task.index import CheckJsonTask
+from tasks.duplicate_name_task.index import DuplicateNameTask
+from tasks.glp_task.index import GLPTask
 # from tasks.lambda_in_local_files_task.index import LambdaInLocalFilesTask
 # from tasks.step_functions_task.index import StepFunctionsTask
 # from tasks.name_length_task.index import NameLengthTask
 # from tasks.unit_test_task.index import UnitTestTask
 # from tasks.path_parameter_task.index import PathParameterTask
-from tasks.api_gateway_task.index import ApiGatewayTask
 # from tasks.environment_variable_task.index import EnvironmentVariableTask
 # from tasks.local_variable_task.index import LocalVarTask
-from tasks.duplicate_name_task.index import DuplicateNameTask
 # from tasks.unused_step_function_task.index import UnusedStepFunctionPermissionsTask
 # from tasks.generic_step_function_task.index import GenericStepFunctionTask
 from commons.taskinterface import Warning
@@ -21,17 +21,17 @@ def main():
     print('\033[92m' + generate_location(3) + '\033[37m') 
 
     tasks = [
-        CheckJsonTask(),
+        # ApiGatewayTask(),
+        # CheckJsonTask(),
+        DuplicateNameTask(),
         # GLPTask(),
         # UnitTestTask(),
         # LambdaInLocalFilesTask(),
         # NameLengthTask(),
         # StepFunctionsTask(),
         # PathParameterTask(),
-        # ApiGatewayTask(),
         # EnvironmentVariableTask(),
         # LocalVarTask(),
-        # DuplicateNameTask(),
         # UnusedStepFunctionPermissionsTask(),
         # GenericStepFunctionTask(),
     ]
