b4a65eb7a41cd1647794e67489ab7ec9f930a268
Caleb Romney Syndergaard <crsyndergaard@churchofjesuschrist.org>
2022-07-18 11:19:19 -0600

    Work on API gateway task

--- tasks/api_gateway_task/index.py
+++ tasks/api_gateway_task/index.py
@@ -25,26 +25,26 @@ class ApiGatewayTask(ValidationTask):
         lambda_arns = set()
         lambda_arns_in_routes = set()
 
-        # for terraform_module in yieldNextModule(dependencies['terraform_dicts'], api_filter):
-        #     for value in terraform_module['module'].values():
-        #         lambda_arns = set(value['lambda_arns'])
-        #         list_of_routes =set([route['lambda_arn'] for route in value['routes']])
-        #         if lambda_arns != lambda_arns_in_routes:
-        #             return list(lambda_arns - lambda_arns_in_routes) + list(lambda_arns_in_routes - lambda_arns)
-        with open(f'{generate_location(3)}/terraform/api-gateway.tf') as file:
+        for terraform_module in yieldNextModule(dependencies['terraform_dicts'], api_filter):
+            for value in terraform_module['module'].values():
+                lambda_arns = set(value['lambda_arns'])
+                list_of_routes =set([route['lambda_arn'] for route in value['routes']])
+                if lambda_arns != lambda_arns_in_routes:
+                    return list(lambda_arns - lambda_arns_in_routes) + list(lambda_arns_in_routes - lambda_arns)
+        # with open(f'{generate_location(3)}/terraform/api-gateway.tf') as file:
 
 
-            obj = hcl.load(file)
-            for key in obj['module']:
-                print(key)
-            # simple-rest-lambda is brittle, find all folders with that name
-            lambda_arns = set(obj['module']['simple-rest-lambda']['lambda_arns'])
-            list_of_routes = obj['module']['simple-rest-lambda']['routes']
-            for route in list_of_routes:
-                lambda_arns_in_routes.add(route['lambda_arn'])
+        #     obj = hcl.load(file)
+        #     for key in obj['module']:
+        #         print(key)
+        #     # simple-rest-lambda is brittle, find all folders with that name
+        #     lambda_arns = set(obj['module']['simple-rest-lambda']['lambda_arns'])
+        #     list_of_routes = obj['module']['simple-rest-lambda']['routes']
+        #     for route in list_of_routes:
+        #         lambda_arns_in_routes.add(route['lambda_arn'])
 
-        if lambda_arns != lambda_arns_in_routes:
-            return list(lambda_arns - lambda_arns_in_routes) + list(lambda_arns_in_routes - lambda_arns)
+        # if lambda_arns != lambda_arns_in_routes:
+        #     return list(lambda_arns - lambda_arns_in_routes) + list(lambda_arns_in_routes - lambda_arns)
 
         return []
 
